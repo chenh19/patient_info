2b10706a7558e5045b79113dcea3b28172393e3b
chen_h <chen_h@outlook.com>
2021-03-30 15:15:28 -0400

    Create patient_info_downloader.R

---
+++ patient_info_downloader.R
@@ -0,0 +1,48 @@
+# install.packages(c("RODBC","filesstrings","dplyr"))
+
+
+# load environment
+lapply(c("RODBC", "filesstrings", "dplyr", "utils"), require, character.only = TRUE)
+
+
+# login with JHED ID
+print(Sys.setenv(JHED = 'JHED_ID')) #Put your JHED here
+print(Sys.setenv(JHED_PWD = rstudioapi::askForPassword("")))
+
+
+# Connect to SQL database
+server="esmpmdbpr4.esm.johnshopkins.edu"
+db_name="CROWNBraunstein_Projection" #Change this
+pwd = Sys.getenv("JHED_PWD")
+stopifnot(nzchar(pwd))
+JHED = Sys.getenv("JHED")
+stopifnot(nzchar(pwd))
+connectionString <- paste0("Driver=freeTDS;TDS_Version=8.0;",'Server=',server,';Port=1433;Database=',
+                           db_name,'; Uid=win\\',Sys.getenv("JHED"),
+                           ';Pwd=',Sys.getenv("JHED_PWD"))
+con <- odbcDriverConnect(connection=connectionString)
+
+
+# Access the tables
+tabs <- sqlTables(con) #Prints out list of tables; use tables w/ dbo schema
+tabs <- filter(tabs, TABLE_TYPE == "TABLE")
+tabs <- filter(tabs, TABLE_SCHEM == "dbo")
+write.table(tabs, file="all_tables.csv", sep=",", row.names = FALSE)
+tab_names <- read.csv("all_tables.csv")[,"TABLE_NAME"]
+
+
+# Download the tables
+dir.create("Tables")
+for (tab_name in tab_names){
+  df <- sqlFetch(con, tab_name)
+  filename <- paste0(tab_name, ".csv")
+  write.table(df, file=filename, sep=",", row.names = FALSE)
+  file.move(filename, "./Tables", overwrite=TRUE)
+  Sys.sleep(1)
+}
+close(con) #close the connection when done
+
+
+# Compress all csv files into one zip file
+files2zip <- dir('Tables', full.name=TRUE)
+zip(zipfile = 'all_tables', files = files2zip)
